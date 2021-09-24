512e9b5ecaaadda5ae4d887be8b9c72507f31d55
matthcol <matthias.colin@gmail.com>
2021-09-24 11:23:04 +0200

    join + group by

---
+++ queriesDay3.sql
@@ -0,0 +1,113 @@
+-- jointures
+
+-- liste des films avec leur réalisateur 
+-- jointure interne : on ne garde que les films ayant des réalisateurs
+select * 
+from 
+	movies m
+	join stars s on m.id_director = s.id
+order by s.id, s.name, m.year;
+
+select m.title, m.year, s.id, s.name 
+from 
+	movies m
+	join stars s on m.id_director = s.id
+order by s.id, s.name, m.year;
+
+select m.*, s.name
+from 
+	movies m
+	join stars s on m.id_director = s.id
+order by s.name, m.year;
+
+-- elements absents de la jointure
+select * from movies where id_director is null;
+
+select * 
+from 
+	movies m
+	join stars s on m.id_director = s.id
+where m.title like 'Tiny Toon Adventures'
+order by s.id, s.name, m.year;
+
+select *
+from 
+	movies m, stars s
+where
+	m.id_director = s.id
+	and m.year between 1970 and 1979;
+	
+-- liste des stars ayant joué James Bond avec le titre et l'année des films
+select *
+from
+	stars s
+	join play p on s.id = p.id_actor
+	join movies m on p.id_movie = m.id
+where 
+	p.role like 'James Bond'
+order by m.year;
+
+-- liste des films de 1990 avec leur réalisateur si renseigné
+-- on garde les films n'ayant pas de réalisateurs
+select *
+from 
+	movies m 
+	left join stars s on m.id_director = s.id
+where m.year = 1990
+order by m.title;
+
+-- toutes les jointures
+-- join = inner join
+-- left join = left outer join
+-- right join = right outer join
+-- full join = full outer join
+-- cross join
+
+select * from stars s
+cross join movies m
+where s.name = 'Clint Eastwood';
+
+-- apparté sur les verrous
+SELECT * 
+FROM sys.dm_exec_requests
+WHERE DB_NAME(database_id) = 'dbmovie' 
+AND blocking_session_id <> 0;
+
+sp_who2
+
+-- liste des réalisateurs avec leur nombre de réalisation
+-- ERROR : La colonne 'movies.title' n'est pas valide dans la liste de sélection parce qu'elle n'est pas contenue dans une fonction d'agrégation ou dans la clause GROUP BY.
+select id_director, title, count(*)
+from movies
+group by id_director;
+
+-- ok
+select id_director, count(*) as nb_movies
+from movies
+group by id_director
+order by nb_movies desc;
+
+select 
+	id_director, 
+	count(*) as nb_movies,
+	min(year) as year_min,
+	max(year) as year_max,
+	string_agg(title, ', ') as titles
+from movies
+group by id_director
+order by nb_movies desc;
+
+select 
+	s.id, s.name,
+	count(*) as nb_movies,
+	min(m.year) as year_min,
+	max(m.year) as year_max,
+	string_agg(m.title, ', ') as titles
+from 
+	stars s join movies m on s.id = m.id_director
+group by s.id, s.name
+order by nb_movies desc;
+
+
+
+
